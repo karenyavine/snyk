9576d6c5742a0a26e1b1bfc86e52430610cec9a9
Yair Zohar <yair.zohar@snyk.io>
2022-01-17 18:55:27 +0200

    feat: CLI Report-Project Envelope PoC

--- src/lib/iac/cli-report.ts
+++ src/lib/iac/cli-report.ts
@@ -2,17 +2,18 @@ import config from '../config';
 import { makeRequest } from '../request';
 import { getAuthHeader } from '../api-token';
 import { FormattedResult } from '../../cli/commands/test/iac-local-execution/types';
+import { convertIacResultsToScanResult } from './utils';
 
-export function sendReport(issues: FormattedResult[]) {
-  makeRequest({
-    method: 'POST',
-    url: `${config.API}/iac-cli-report`,
-    json: true,
-    headers: {
-      authorization: getAuthHeader(),
-    },
-    body: {
-      issues,
-    },
+export function sendReport(results: FormattedResult[]) {
+  results.forEach((result) => {
+    makeRequest({
+      method: 'PUT',
+      url: `${config.API}/monitor-dependencies`,
+      json: true,
+      headers: {
+        authorization: getAuthHeader(),
+      },
+      body: { scanResult: convertIacResultsToScanResult(result) },
+    });
   });
 }
